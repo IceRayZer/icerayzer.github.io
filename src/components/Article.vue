ce03345eabb4f36b350987e0cd307d7da379c48a
IceRayZer <icerayzer@gmail.com>
2024-08-21 19:10:07 +0200

    Ajout h2 + remplacement des titre h1 en h2

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -15,7 +15,7 @@ withDefaults(defineProps<ArticleProps>(), {
 
 <template>
   <div class="page">
-    <h1>{{ $props.title }}</h1>
+    <h2>{{ $props.title }}</h2>
     <div class="content ql-editor">
       <div class="infos">
         <div class="tags">
